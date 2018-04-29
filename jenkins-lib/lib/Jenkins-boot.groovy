c19c376b740c128a295f6fa8d48d650bbe67aa91
wuzhaozhongguo <13991544720@139.com>
2018-04-29 17:19:45 +0800

    pipline

--- jenkins-lib/lib/Jenkins-boot.groovy
+++ jenkins-lib/lib/Jenkins-boot.groovy
@@ -18,11 +18,12 @@ pipeline {
         stage('Build') {
             steps {
                 sh "mvn clean package install -Dmaven.test.skip=true -pl ${BUILD_ROOT_PATH}/${SERVICE_NAME}/"
-
             }
         }
         stage('Stash'){
-            stash includes: "${BUILD_ROOT_PATH}/${SERVICE_NAME}/target/${SERVICE_NAME}-${SERVICE_VERSION}.jar", name:"${SERVICE_NAME}-${SERVICE_VERSION}.jar"
+            steps {
+                stash includes: "${BUILD_ROOT_PATH}/${SERVICE_NAME}/target/${SERVICE_NAME}-${SERVICE_VERSION}.jar", name:"${SERVICE_NAME}-${SERVICE_VERSION}.jar"
+            }
         }
     }
     post {
