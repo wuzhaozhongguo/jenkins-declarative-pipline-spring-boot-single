e561802273976a3228a58cf3d0f00b6b67de3b34
wuzhaozhongguo <wuzhaozhongguo@hotmail.com>
2018-05-15 20:04:35 +0800

    上传

--- jenkins-lib/lib/Jenkins-boot.groovy
+++ jenkins-lib/lib/Jenkins-boot.groovy
@@ -4,32 +4,35 @@ SCM_URL="git@10.50.10.214:jcpt/caifubao-jcpt.git"
 SCM_BRANCH="test"
 BUILD_ROOT_PATH="caifubao-service/"
 pipeline {
+    agent none
     tools {
         jdk 'java1.8'
         maven 'maven3'
     }
         stages {
             stage('Checkout') {
+                agent{node { label 'master' }}
                 steps {
                     git branch: "${SCM_BRANCH}", credentialsId: 'wuzhao', url: "${SCM_URL}"
                 }
             }
             stage('Build') {
+                agent{node { label 'master' }}
                 steps {
                     sh "mvn clean package install -Dmaven.test.skip=true -pl ${BUILD_ROOT_PATH}/${SERVICE_NAME}/"
                 }
             }
             stage('Stash'){
+                agent{node { label 'master' }}
                 steps {
                     stash includes: "${BUILD_ROOT_PATH}/${SERVICE_NAME}/target/*.jar", name:"${SERVICE_NAME}"
                 }
             }
 
             stage('UPLOAD') {
+                agent{node { label 'test' }}
                 steps {
-                    node('test'){
-                        unstash "${SERVICE_NAME}"
-                    }
+                    unstash "${SERVICE_NAME}"
                 }
             }
         }
